98d3c53248a025ecfb9b845c640bc5b17b100ab3
Edwin Yung <eyung7@gmail.com>
2020-06-16 03:07:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ThirdPersonMP
+
+Developed with Unreal Engine 4
---
+++ Source/ThirdPersonMP/ThirdPersonMPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonMPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThirdPersonMPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThirdPersonMPGameMode();
+};
+
+
+
